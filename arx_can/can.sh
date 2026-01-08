b84c23e297a45c2f695d724b65fa48139167ac17
zionshang <zishangji@qq.com>
2026-01-08 14:34:42 +0800

    重构CAN相关脚本，简化启动和配置流程

--- arx_can/can.sh
+++ arx_can/can.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
 
-workspace=$(pwd)
-
 source ~/.bashrc
 
+CAN_DEVICE="/dev/arxcan0"
+CAN_INTERFACE="can0"
 
-gnome-terminal -t "can1" -x sudo bash -c "cd ${workspace}/arx_can; ./arx_can0.sh; exec bash;"
+echo "启动 slcand..."
+sudo slcand -o -f -s8 $CAN_DEVICE $CAN_INTERFACE
+if [ $? -ne 0 ]; then
+    echo "slcand 启动失败"
+    exit 1
+fi
+
+echo "配置 $CAN_INTERFACE 接口..."
+sudo ifconfig $CAN_INTERFACE up
+
+if [ $? -ne 0 ]; then
+    echo "启动 $CAN_INTERFACE 接口失败"
+    exit 1
+fi
+
+echo "$CAN_INTERFACE 启动成功"
--- arx_can/set.sh
+++ arx_can/set.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-workspace=$(pwd)
-
 source ~/.bashrc
 
-
-
- gnome-terminal -t "master1" -x sudo bash -c "cd ${workspace}; sudo cp arx_can.rules /etc/udev/rules.d/;sleep 1;sudo chmod +x /etc/udev/rules.d/arx_can.rules;sleep 1;sudo udevadm control --reload-rules && sudo udevadm trigger; exec bash;"
+sudo cp arx_can.rules /etc/udev/rules.d/
+sudo chmod +x /etc/udev/rules.d/arx_can.rules
+sudo udevadm control --reload-rules && sudo udevadm trigger
+echo "Configured CAN rules."
 
 
